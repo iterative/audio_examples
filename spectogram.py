5a94980ca2df523dcbc676e3350c72efd2cc907f
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2025-07-30 15:56:07 -0700

    add spectogram image

--- spectogram.py
+++ spectogram.py
@@ -2,14 +2,11 @@ import io
 
 import librosa
 import numpy as np
-from tempfile import TemporaryDirectory
-from pathlib import Path
 
-from pydub import AudioSegment
 import datachain as dc
 from datachain.lib.audio import audio_to_bytes
 from datachain import AudioFile, File
-
+import matplotlib.pyplot as plt
 
 LOCAL = True
 BUCKET = "data-flac/datachain-usw2-main-dev" if LOCAL else "s3://datachain-usw2-main-dev"
@@ -17,6 +14,39 @@ INPUT_DIR = f"balanced_train_segments/audio"
 INPUT_PATH = f"{BUCKET}/test/{INPUT_DIR}"
 OUTPUT_PATH = f"{BUCKET}/test/{INPUT_DIR}/spectrograms"
 OUTPUT = "spectrogram"
+
+
+def create_spectrogram(file: AudioFile):
+    audio_bytes = audio_to_bytes(file, format="mp3")
+    with io.BytesIO(audio_bytes) as f:
+        y, sr = librosa.load(f)
+
+    # Compute the spectrogram using Short-Time Fourier Transform (STFT)
+    spectrogram = np.abs(librosa.stft(y))
+
+    # Convert amplitude spectrogram to decibels for better interpretation
+    spectrogram_db = librosa.amplitude_to_db(spectrogram, ref=np.max)
+    return spectrogram_db, sr
+
+
+def create_spectrogram_image(file: AudioFile) -> File:
+    spectrogram_db, sr = create_spectrogram(file)
+    f = plt.figure(figsize=(10, 6))
+    librosa.display.specshow(spectrogram_db, x_axis='time', y_axis='mel', sr=sr, cmap='viridis')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel Spectrogram')
+    plt.tight_layout()
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, dpi=300, bbox_inches='tight')
+    plt.close()
+    buffer.seek(0)
+    img_bytes = buffer.read()
+
+    # Upload data and return it as File structure
+    save_path = f"{OUTPUT_PATH}/{file.get_file_stem()}.jpg"
+    spectrum_file = File.upload(img_bytes, save_path)
+    return spectrum_file
 
 
 def create_spectrogram_data(file: AudioFile) -> File:
@@ -32,22 +62,8 @@ def create_spectrogram_data(file: AudioFile) -> File:
     Returns:
         numpy.ndarray: The computed spectrogram (magnitude or power) in decibels.
     """
-    audio = AudioSegment.from_file(file.get_local_path(), format="mp3")
-    audio_bytes = audio_to_bytes(file, format="mp3")
 
-    with TemporaryDirectory() as temp_dir:
-        temp_file_path = Path(temp_dir, "audio.mp3")  # or .mp3, .ogg etc.
-        temp_file_path.write_bytes(audio_bytes)
-        y, sr = librosa.load(temp_file_path)
-    # Load the audio file
-    # y, sr = librosa.load(audio_filepath)
-
-    # Compute the spectrogram using Short-Time Fourier Transform (STFT)
-    spectrogram = np.abs(librosa.stft(y))
-
-    # Convert amplitude spectrogram to decibels for better interpretation
-    spectrogram_db = librosa.amplitude_to_db(spectrogram, ref=np.max)
-
+    spectrogram_db, sr = create_spectrogram(file)
     # Convert to bytes in numpy format
     buffer = io.BytesIO()
     np.save(buffer, spectrogram_db)
@@ -56,6 +72,7 @@ def create_spectrogram_data(file: AudioFile) -> File:
     # Upload data and return it as File structure
     save_path = f"{OUTPUT_PATH}/{file.get_file_stem()}.npy"
     spectrum_file = File.upload(npy_bytes, save_path)
+
     return spectrum_file
 
 
@@ -63,12 +80,9 @@ result = (
     dc
     .read_storage(INPUT_PATH, type="audio")
     .filter(dc.C("file.path").glob("*.mp3"))
-    # .limit(10)
+    .settings(parallel=8)
+    .limit(10)
     .map(spec=create_spectrogram_data)
+    .map(spec_img=create_spectrogram_image)
     .save(OUTPUT)
 )
-
-if not LOCAL:
-    dc.read_storage(BUCKET, update=True).exec()
-else:
-   result.show()
