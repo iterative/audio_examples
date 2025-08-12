f8fa058300cb9de7964a26a4db5ea86bd372a0ec
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2025-08-11 19:08:44 -0700

    improvements

--- video-fragments.py
+++ video-fragments.py
@@ -2,44 +2,36 @@ from typing import Iterator
 
 import datachain as dc
 from datachain import VideoFile, ImageFile, VideoFragment
-from datachain.model.ultralytics import YoloBBoxes, YoloSegments, YoloPoses
-
-from pydantic import BaseModel
-from ultralytics import YOLO
 
 local = False
 bucket = "data-video" if local else "s3://datachain-usw2-main-dev"
 input_path = f"{bucket}/balanced_train_segments/video"
 output_path = f"{bucket}/temp/video-fragments"
-detection_dataset = "yolo-frames-detector"
-segment_duration = 5
+fragments_dataset = "video-fragments"
+segment_duration = 7
 
 
-def video_fragnebts(file: VideoFile) -> Iterator[VideoFragment]:
-    yield from file.get_fragments(segment_duration)
+class VideoClip(VideoFile):
+    orig: VideoFragment
+
+
+def video_fragments(file: VideoFile) -> Iterator[VideoClip]:
+    for fragment in file.get_fragments(segment_duration):
+        clip = fragment.save(output_path)
+        yield VideoClip(**clip.model_dump(), orig=fragment)
 
 
 chain = (
     dc
     .read_storage(input_path, type="video")
     .filter(dc.C("file.path").glob("*.mp4"))
-    .limit(2)
+    .sample(10)
     .settings(parallel=5)
 
-    .gen(frame=extract_frames)
+    .gen(clip=video_fragments)
 
-    # Initialize models: once per processing thread
-    .setup(
-        yolo=lambda: YOLO("yolo11n.pt"),
-        yolo_segm=lambda: YOLO("yolo11n-seg.pt"),
-        yolo_pose=lambda: YOLO("yolo11n-pose.pt")
-    )
-
-    # Apply yolo detector to frames
-    # .map(bbox=process_bbox)
-    .map(yolo=video_segments)
-    .order_by("frame.path", "frame.num")
-    .save(detection_dataset)
+    .order_by("clip.path", "clip.orig.start")
+    .save(fragments_dataset)
 )
 
 if local:
