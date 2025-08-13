04635f44320c6c8626595023687c575be55c9a5e
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2025-08-13 02:56:08 -0700

    make yolo download stable

--- video-detector.py
+++ video-detector.py
@@ -1,19 +1,39 @@
+import os
 from typing import Iterator
 
 import datachain as dc
-from datachain import VideoFile, ImageFile, func
+from datachain import VideoFile, ImageFile
 from datachain.model.ultralytics import YoloBBoxes, YoloSegments, YoloPoses
 
 from pydantic import BaseModel
-from ultralytics import YOLO
+from ultralytics import YOLO, settings
 
 local = False
 bucket = "data-video" if local else "s3://datachain-usw2-main-dev"
 input_path = f"{bucket}/balanced_train_segments/video"
 output_path = f"{bucket}/temp/video-detector-frames"
 detection_dataset = "frames-detector"
-detection_humans = "human-videos"
 target_fps = 1
+
+model_bbox = "yolo11n.pt"
+model_segm = "yolo11n-seg.pt"
+model_pose = "yolo11n-pose.pt"
+
+
+# Upload models to avoid YOLO-downloader issues
+if not local:
+    weights_dir = f"{os.getcwd()}/{settings['weights_dir']}"
+    dc.read_storage([
+        f"{bucket}/models/{model_bbox}",
+        f"{bucket}/models/{model_segm}",
+        f"{bucket}/models/{model_pose}",
+    ]
+    ).to_storage(weights_dir, placement="filename")
+
+    model_bbox = f"{weights_dir}/{model_bbox}"
+    model_segm = f"{weights_dir}/{model_segm}"
+    model_pose = f"{weights_dir}/{model_pose}"
+
 
 class YoloDataModel(BaseModel):
     bbox: YoloBBoxes
@@ -24,7 +44,6 @@ class YoloDataModel(BaseModel):
 class VideoFrameImage(ImageFile):
     num: int
     orig: VideoFile
-
 
 
 def extract_frames(file: VideoFile) -> Iterator[VideoFrameImage]:
@@ -52,34 +71,20 @@ def process_bbox(yolo: YOLO, frame: ImageFile) -> YoloBBoxes:
     return YoloBBoxes.from_results(yolo(frame.read(), verbose=False))
 
 
-# Ultralitics YOLO download is not stable
-def yolo_with_retry(model: str) -> YOLO:
-    max_retries = 7
-    delay_seconds = 2
-
-    for attempt in range(1, max_retries + 1):
-        try:
-            return YOLO(model)
-        except Exception as e:
-            if attempt < max_retries:
-                time.sleep(delay_seconds)
-            else:
-                raise e
-
 chain = (
     dc
     .read_storage(input_path, type="video")
     .filter(dc.C("file.path").glob("*.mp4"))
-    .sample(7)
+    .sample(2)
     .settings(parallel=5)
 
     .gen(frame=extract_frames)
 
     # Initialize models: once per processing thread
     .setup(
-        yolo=lambda: yolo_with_retry("yolo11n.pt"),
-        yolo_segm=lambda: yolo_with_retry("yolo11n-seg.pt"),
-        yolo_pose=lambda: yolo_with_retry("yolo11n-pose.pt")
+        yolo=lambda: YOLO(model_bbox),
+        # yolo_segm=lambda: YOLO(model_segm),
+        # yolo_pose=lambda: YOLO(model_pose)
     )
 
     # Apply yolo detector to frames
